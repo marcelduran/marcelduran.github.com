723faa6eee281219f1d3ec852e119a71b5c50a68
Marcel Duran <contact@marcelduran.com>
2012-03-08 18:07:40 -0800

    evil removed ;-)

--- _includes/resume.js
+++ _includes/resume.js
@@ -1,24 +1,7 @@
-(function (d, t) {
-    var h = d.getElementsByTagName(t)[0],
-        s = d.createElement(t);
-
-    // set ga
-    s.src = 'http://www.google-analytics.com/ga.js';
-    s.async = 1;
-    _gaq = [
-        ['_setAccount', 'UA-3587044-1'],
-        ['_trackPageview'],
-        ['_trackPageLoadTime']
-    ];
-
-    // insert ga
-    setTimeout(function () {
-        h.parentNode.insertBefore(s, h);
-    }, 0);
-
+(function () {
     // print button
-    d.getElementById('prt').onclick = function (e) {
+    document.getElementById('prt').onclick = function (e) {
         e.preventDefault ? e.preventDefault() : e.returnValue = 0;
         print();
     }
-}(document, 'script'));
+}());
